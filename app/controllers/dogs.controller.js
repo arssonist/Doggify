e55f710ae43e98f44ff3ca10722cc52e1f54c5e2
Your Name <arssonist@yahoo.com>
2017-03-18 20:50:55 -0400

    add second route; add second controller and view;working, but need make local for title; causing error

---
+++ app/controllers/dogs.controller.js
@@ -0,0 +1,16 @@
+module.exports = {
+  showDogs: (req,res) => {
+    const dogs = [
+      {
+        name:"Fluffy", breed:"ChowChow", slug:"fluffy", description:"4 year old Chow. Really, really fluffy."
+      },
+      {
+        name:"Buddy", breed:"White Lab", slug:"buddy", description:"A friendly 6 year old white lab mix. Loves playing ball"
+    },
+    {
+      name: "Derbis", breed:"Schmerbis",slug:"derbis", description:"A real Schmerbis Derbis"
+    }
+  ];
+  res.render("pages/dogs", {dogs: dogs});
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,12 @@
 const express = require('express'),
     router = express.Router(),
 // import from from controller
-    mainController = require('./controllers/main.controller');
+    mainController = require('./controllers/main.controller'),
+    dogsController = require('./controllers/dogs.controller')
 
 
 module.exports = router;
 
 // input function from controller
 router.get('/', mainController.showHome)
+router.get('/dogs', dogsController.showDogs)
